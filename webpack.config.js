5b04ccd7fdea6a2de1ab2211cfdbea07a6227808
Benedikt Rötsch <benedikt@contentful.com>
2017-09-06 11:55:11 +0200

    refactor(bundling): upgrade to webpack 3 and contentful.js v5

--- webpack.config.js
+++ webpack.config.js
@@ -14,13 +14,13 @@ module.exports = {
       {
         test: /\.js?$/,
         exclude: /(node_modules|bower_components|dist)/,
-        loader: 'babel'
+        loader: 'babel-loader'
       },
       {
         test: /\.css$/,
         loaders: [
-          'style?sourceMap',
-          'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
+          'style-loader?sourceMap',
+          'css-loader?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
         ]
       },
       { test: /\.jpe?g$|\.svg$|\.png$/,
